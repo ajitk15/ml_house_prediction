183ed00f0312c3465e3e87ba147be3e311834d4a
Ajit Kumar <ajitk15@gmail.com>
2023-10-22 01:10:21 +0530

    app update

--- app.py
+++ app.py
@@ -1,38 +1,40 @@
+import json
 import pickle
-from flask import Flask, request, app, jsonify, url_for, render_template
-import pandas as pd
+
+from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
-import bz2file as bz2
-import os
+import pandas as pd
 
-#Initiate Flask
 app=Flask(__name__)
-
-#Load model
-scaler=pickle.load(open('StandardScaler.pkl','rb'))
-model=pickle.load(open('house_pred_model.pkl','rb'))
+## Load the model
+regmodel=pickle.load(open('house_pred_model.pkl','rb'))
+scalar=pickle.load(open('StandardScaler.pkl','rb'))
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/predict_api', methods=['POST'])
+@app.route('/predict_api',methods=['POST'])
 def predict_api():
     data=request.json['data']
-    print(np.array(list(data.values())).reshape(1, -1))
-    new_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
-    new_pred = model.predict(new_data)
-    print(new_pred[0])
-    return jsonify(new_pred[0])
-    
-@app.route('/predict', methods=['POST'])
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    output=regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
 def predict():
     data=[float(x) for x in request.form.values()]
-    print(np.array(data).reshape(1, -1))
-    new_data = scaler.transform(np.array(data).reshape(1, -1))
-    new_pred = model.predict(new_data)
-    print(new_pred[0])
-    return render_template('home.html', prediction_text=f"Esitmated Home price {new_pred[0]}")
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
 
-if __name__ == "__main__":
+
+
+if __name__=="__main__":
     app.run(debug=True)
+   
+     
