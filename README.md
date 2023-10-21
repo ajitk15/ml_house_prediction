24c30dea25ff835e58b0ef10df9b6bb8a217f2a7
Ajit Kumar <ajitk15@gmail.com>
2023-10-21 23:16:47 +0530

    Inital Commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# ml_house_prediction
+### California House Pricing Predeiction
+
+### Software and tools 
+
+1. [Github Account] (https://github.com)
+2. [Heroku Account] (https://heroku.com)
+3. [VC Code IDE] (https://code.visualstudio.com/)
+4. [Git CLI] (https://git-scm.com/downloads)
+
+
+
+Create New environment
+
+```
+conda create -p venv python==3.7 -y
+
+conda activate venv
+```
---
+++ app.py
@@ -0,0 +1,48 @@
+import pickle
+from flask import Flask, request, app, jsonify, url_for, render_template
+import pandas as pd
+import numpy as np
+import bz2file as bz2
+
+#Initiate Flask
+app=Flask(__name__)
+
+#Load Compressed pickel file
+#data1 = bz2.BZ2File('house_pred_model.pbz2','rb')
+#model = pickle.load(data1)
+
+def decompress_pickle(file):
+    data=bz2.BZ2File(open(file, 'rb'))
+    data = pickle.load(data)
+    return data
+
+model = decompress_pickle('house_pred_model.pbz2')
+
+#Load model
+scaler=pickle.load(open('StandardScaler.pkl','rb'))
+#model=pickle.load(open('house_pred_model.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(np.array(list(data.values())).reshape(1, -1))
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
+    new_pred = model.predict(new_data)
+    print(new_pred[0])
+    return jsonify(new_pred[0])
+    
+@app.route('/predict', methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    print(np.array(data).reshape(1, -1))
+    new_data = scaler.transform(np.array(data).reshape(1, -1))
+    new_pred = model.predict(new_data)
+    print(new_pred[0])
+    return render_template('home.html', prediction_text=f"Esitmated Home price {new_pred[0]}")
+
+if __name__ == "__main__":
+    app.run(debug=True)
